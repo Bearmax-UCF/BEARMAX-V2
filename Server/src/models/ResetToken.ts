74ba5233975734f0902810e0b636d996d2fb37da
Jason Lin <jasonlinfl@gmail.com>
2023-11-28 12:22:51 -0500

    Implement forgot password api

---
+++ Server/src/models/ResetToken.ts
@@ -0,0 +1,49 @@
+import { Model, Schema, Types, HydratedDocument, model } from "mongoose";
+import bcrypt from "bcrypt";
+import constants from "../utils/constants";
+
+interface IResetToken {
+    userId: string;
+    token: string;
+    createdAt: Date;
+}
+
+interface ResetTokenMethods extends Model<IResetToken> {
+    /** Deletes the reset token from the database
+    */
+    deleteResetToken(): void;
+    /** Verifies the reset token for comparison
+    */
+    verifyResetToken(token: string): Promise<boolean>;
+}
+
+interface ResetTokenModel extends Model<IResetToken, {} ,ResetTokenMethods> {
+}
+
+const ResetTokenSchema = new Schema<IResetToken, ResetTokenModel, ResetTokenMethods>({
+    userId: { type: String, required: true },
+    token: { type: String, required: true },
+    createdAt: { type: Date, expires: 3600, default: Date.now },
+});
+
+ResetTokenSchema.pre("save", function (next) {
+    bcrypt.genSalt(constants.bcrypt_log_rounds, (err, salt) => {
+        bcrypt.hash(this.token, 10, (err, hash) => {
+            if (err) return next(err);
+            this.token = hash;
+            next();
+        });
+    });
+});
+
+ResetTokenSchema.methods.deleteResetToken = async function () {
+    await this.deleteOne({ _id: this.userId })
+}
+
+ResetTokenSchema.methods.verifyResetToken = async function (token: string) {
+    return await bcrypt.compare(token, this.token);
+}
+
+const ResetToken = model<IResetToken, ResetTokenModel>("ResetToken", ResetTokenSchema);
+
+export default ResetToken;
--- Server/src/routes/api/auth.ts
+++ Server/src/routes/api/auth.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ExtractJwt } from "passport-jwt";
 import requireLocalAuth from "../../middleware/requireLocalAuth";
 import AuthToken from "../../models/AuthToken";
+import ResetToken from "../../models/ResetToken";
 import User from "../../models/User";
 import jwt from "jsonwebtoken";
 import constants from "../../utils/constants";
@@ -64,15 +65,72 @@ router.post("/register", async (req, res, next) => {
 	}
 });
 
+router.post("/forgotPasswordRequest", async (req, res, next) => {
+	const email = req.body.email;
+	if (!email) 
+		return res.status(400).send({ message: "Missing email field." });
+	try {
+		const user = await User.findOne({ email });
+		if(!user)
+			return res.status(422).send({ message: "User not found." });
+		const unhashToken = crypto.randomBytes(32).toString('hex');
+		
+		await new ResetToken({ userId: user._id, token: unhashToken }).save();
+		mailgun.send(email, 
+			'Password Reset',
+			 `<h1>Hello!</h1>
+			 <p> Please reset your bearmax account password by clicking the following link: http://localhost:8080/api/auth/resetPassword?token=${unhashToken}&id=${user._id}</p>
+			 `
+			 )
+		.catch((err) => console.log(err));
+		res.status(201).send({ message: "Password Reset Request Sent Successfully!" });
+	} catch (error) {
+		return next(error);
+	}
+});
+
+router.post("/resetPassword", async (req, res, next) => {
+	const token = req.body.token;
+	const id = req.body.id;
+	const newPassword = req.body.password;
+	if (!token || !id || !newPassword) 
+		return res.status(400).send({ message: "Missing one or more fields." });
+	try {
+		const resetToken = await ResetToken.findOne({ userId: id });
+		if(!resetToken)
+			return res.status(422).send({ message: "Reset token not found." });
+		const compare = await resetToken.verifyResetToken(token);
+		if(compare) {
+			const user = await User.findById(id);
+			if(!user)
+				return res.status(422).send({ message: "User not found." });
+			// comparePassword
+			const isMatch = await user.comparePassword(newPassword);
+			if(!isMatch) {
+				user.password = newPassword;
+				user.save();
+				resetToken.deleteResetToken();
+				return res.status(201).send({ message: "Password Reset Successfully!" });
+			} else {
+				return res.status(422).send({ message: "New password cannot be the same as the old password." });
+			}
+		} else {
+			return res.status(422).send({ message: "Invalid token." });	
+		}
+	} catch (error) {
+		return next(error);
+	}
+});
+
 router.get("/verify", async (req, res, next) => {
 	try {
 		const token = req.query.token as string;
 		const id = req.query.id as string;
 		const user = await User.findById(id);
 		if(!user)
-			return res.status(400).send({ message: "User not found." });
+			return res.status(422).send({ message: "User not found." });
 		if(user.isVerified) 
-			return res.status(400).send({ message: "User already verified." });
+			return res.status(422).send({ message: "User already verified." });
 		if(token) {
 			const compare = await bcrypt.compare(token, user.hashToken as string);
 			if(compare) {
@@ -81,8 +139,10 @@ router.get("/verify", async (req, res, next) => {
 				user.save();
 				return res.status(201).send({ message: "User verified!" });
 			} else {
-				return res.status(400).send({ message: "Invalid token." });
+				return res.status(422).send({ message: "Invalid token." });
 			}
+		} else {
+			return res.status(400).send({ message: "Missing token." });
 		}
 	} catch (error) {
 		return next(error);
