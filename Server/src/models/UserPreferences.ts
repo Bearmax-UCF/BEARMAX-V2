91d5a14c56500452e65777eaa1da40f702980016
Jason Lin <jasonlinfl@gmail.com>
2024-01-23 13:58:26 -0500

    add a collection to each model created

--- Server/src/models/UserPreferences.ts
+++ Server/src/models/UserPreferences.ts
@@ -5,6 +5,9 @@ const UserPreferences = new mongoose.Schema(
         UserID: { type: String, required: true },
         Video: { type: Boolean, required: true },
         Audio: { type: Boolean, required: true },
+    },
+    {
+        collection: "UserPreferences"
     }
 );
 
