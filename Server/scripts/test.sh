0a12f112031a623797ef811100cf6ca59982943b
Jason Lin <jasonlinfl@gmail.com>
2024-01-26 11:59:33 -0500

    implement azure connection to user

--- Server/scripts/test.sh
+++ Server/scripts/test.sh
@@ -5,9 +5,9 @@ docker-compose run -e PORT=0 backend test
 
 # Perform the test here...
 
-# Delete the Docker Compose containers and images
-docker stop $(docker ps -a -q)
+# Delete the Docker Compose containers and images and suppresses output from it
+docker stop $(docker ps -a -q) 1> /dev/null
 
-docker container prune -f
-docker rmi bearmax/backend:latest
-docker volume prune -a -f
+docker container prune -f 1> /dev/null
+docker rmi bearmax/backend:latest 1> /dev/null
+docker volume prune -a -f 1> /dev/null
