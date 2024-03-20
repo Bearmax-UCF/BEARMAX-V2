108cbe8429410c433740b464bb04783ab8e242ff
ZackOrooji <zackorooji11@gmail.com>
2024-03-20 17:54:40 -0400

    Notes feature completed functionally

---
+++ Client/src/components/CreateNote.vue
@@ -0,0 +1,112 @@
+<template>
+
+    <h1>This is the Create Note page</h1>
+
+    <!-- <p>Head back to View Patient Notes page</p>
+
+    <router-link to="/viewpatientnotes">
+
+        <font-awesome-icon icon="plus" size="2x" style=""/>
+
+    </router-link> -->
+
+    <br>
+
+    <!-- Create note test -->
+
+    <input type="text" class="textField" placeholder="title" v-model="newNote.title"/>
+
+    <br>
+
+    <input type="text" class="textField" placeholder="Date" v-model="newNote.date"/>
+
+    <br>
+
+    <!-- <input type="text" class="textField" placeholder="Note" v-model="newNote.note" 
+    style="width:600px; height:300px; align-items:flex-start; display:flex;" /> -->
+
+    <textarea v-model="newNote.note" placeholder="Note" style="width:600px; height:300px; position:relative; 
+        resize:none;">
+
+    </textarea>
+
+    <br>
+
+    <button style= "position:relative;" @click="createNote"> Create Note </button> 
+
+    <!-- End of create note test -->
+
+
+
+</template>
+
+
+<script>
+
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+    import { ref } from 'vue';
+    import { createNoteApi} from '@/api';
+    import { useRouter} from 'vue-router';
+
+
+    export default {
+
+        setup() {
+
+            const router = useRouter();
+
+            const newNote = ref ({
+
+                title: '',
+                date: '',
+                note: ''
+            });
+
+            const createNoteError = ref(null);
+
+            const createNote = async () => {
+
+                try{
+                    
+                    console.log('calling createNoteApi function ');
+                    const response = await createNoteApi(newNote.value);
+
+                    console.log("response is");
+                    console.log(response);
+
+                    console.log("response. is")
+                    console.log("");
+
+
+                    console.log('CreateNoteApi call returned.');
+                    router.push('./viewpatientnotes');
+
+                }
+
+                catch (error) {
+                    console.log("failed");
+                    createNoteError.value = error.message || 'An error occured.';
+                }
+
+            }//End of createNote function
+
+
+            return {
+
+                newNote,
+                createNote,
+                createNoteError
+
+            };//End of return
+
+        },//End of setup
+
+    }//End of export default   
+
+
+</script>
+
+<style>
+
+
+</style>
---
+++ Client/src/components/EditNote.vue
@@ -0,0 +1,94 @@
+<template>
+
+    <h1>This is the Edit note page</h1>
+
+    
+
+
+
+
+
+
+    <p>Head back to View Patient Notes page</p>
+
+    <router-link to="/viewpatientnotes">
+        <font-awesome-icon icon="pencil-alt" size="2x" style=""/>
+    </router-link>
+
+</template>
+
+
+<script>
+
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+    import { ref } from 'vue';
+    import { createNoteApi} from '@/api';
+    import { useRouter} from 'vue-router';
+
+
+    export default {
+
+        setup() {
+
+            const router = useRouter();
+
+            const noteToBeEdited = ref ({
+
+                title: '',
+                date: '',
+                note: ''
+            });
+
+            const editNoteError = ref(null);
+
+            const editNote = async () => {
+
+                try{
+                    
+                    console.log('calling createNoteApi function ');
+                    const response = await createNoteApi(newNote.value);
+
+                    console.log("response is");
+                    console.log(response);
+
+                    console.log("response. is")
+                    console.log("");
+
+
+                    console.log('EditNoteApi call returned.');
+                    router.push('./viewpatientnotes');
+
+                }
+
+                catch (error) {
+                    console.log("failed");
+                    editNoteError.value = error.message || 'An error occured.';
+                }
+
+            }//End of editNote function
+
+
+            return {
+
+                noteToBeEdited,
+                editNote,
+                editNoteError
+
+            };//End of return
+
+        },//End of setup
+
+    }//End of export default 
+
+
+
+
+
+
+</script>
+
+<style>
+
+
+</style>
