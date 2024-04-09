bc50ee64cd21b2205a5b51b28ed46b268e529606
Sabrina-Lopez <sa568892@ucf.edu>
2024-04-09 12:42:25 -0400

    dealt with other conflicts ;-;

--- Server/test-client.ts
+++ Server/test-client.ts
@@ -56,13 +56,9 @@ socket.on("connect", () => {
 	socket.emit("emotionGame", "stop", { userID: USERID });
 	socket.emit("recalibrate");
 	socket.emit("GSR", JSON.stringify({ value: 450, ts: new Date() }));
-<<<<<<< HEAD
-	socket.emit("playMedia", JSON.stringify({ mediaURL: "Metal_pipe_falling_sound_effectloud.mp4" }), { userID: USERID });
-=======
 	//socket.emit("playMedia", JSON.stringify({ mediaName: "Metal_pipe_falling_sound_effectloud.mp4", videoBool: true, audioBool: false }), { userID: USERID });
 	//socket.emit("playMedia", JSON.stringify({ mediaName: "Metal_pipe_falling_sound_effectloud.mp4", videoBool: true, audioBool: true }), { userID: USERID });
 	socket.emit("playMedia", JSON.stringify({ mediaName: "Metal_pipe_falling_sound_effect_but_its_more_violent.mp3", videoBool: false, audioBool: true }), { userID: USERID });
->>>>>>> parent of 71b65dd (trying to get pull request to work)
 });
 
 
